d9a3c61f476b6b1c50096f8e3528aff8195d81ad
Eslam-nasser-wd <in1146@fayoum.edu.eg>
2017-11-12 16:30:50 +0200

    Fix nav issue

--- js/main.js
+++ js/main.js
@@ -11,70 +11,6 @@ $(document).ready(function(){
   }
 });
 
-$(window).scroll(function(){
-  var scrollTop         = $(document).scrollTop()
-      heightNav         = $('nav').outerHeight(true)
-      sliderHeight      = $('.slider').height()
-      prouductsOffset   = sliderHeight - heightNav // 405
-      prouductsHeight   = $('.prouducts').height() // 2560
-      aboutOffset       = $('.about-us').offset().top - heightNav  // 3892
-      aboutHeight       = $('.about-us').outerHeight(true) // 550
-      contactOffset     = $('.contact').offset().top - heightNav // 550 + 3892 = 4442
-
-  if( scrollTop < sliderHeight){
-    $('li').removeClass('active')
-    $('li.home-nav').addClass('active')
-  } // addClass for li.home-nav
-  else if( scrollTop > prouductsOffset && scrollTop < aboutOffset){
-    $('li').removeClass('active')
-    $('li.prouducts-nav').addClass('active')
-  } // addClass for li.prouducts-nav
-  else if( scrollTop > aboutOffset && scrollTop < contactOffset){
-    $('li').removeClass('active')
-    $('li.about-us-nav').addClass('active')
-  } // addClass for li.about-us-nav
-  else if( scrollTop > contactOffset){
-    $('li').removeClass('active')
-    $('li.contact-nav').addClass('active')
-  } // addClass for li.contact-nav
-})
-
-
-// This function work when user clicked on (li.home-nav) and give him class active #1
-$('li.home-nav').click(function(){
-  $('html, body').animate({scrollTop: 0}, 1000)
-  $('li').removeClass('active')
-  $(this).addClass('active')
-})
-// This function work when user clicked on (li.prouducts-nav) and give him class active #2
-$('li.prouducts-nav').click(function(){
-  var scrollTop         = $(document).scrollTop()
-      sliderHeight      = $('.slider').height()
-      prouductsOffset   = sliderHeight // 405
-
-  $('html, body').animate({scrollTop: prouductsOffset}, 1000)
-  $('li').removeClass('active')
-  $(this).addClass('active')
-})
-// This function work when user clicked on (li.about-us-nav) and give him class active #3
-$('li.about-us-nav').click(function(){
-  var scrollTop         = $(document).scrollTop()
-      heightNav         = $('nav').outerHeight(true)
-      aboutOffset       = $('.about-us').offset().top - heightNav // 3892
-
-  $('html, body').animate({scrollTop: aboutOffset}, 1000)
-  $('li').removeClass('active')
-  $(this).addClass('active')
-})
-// This function work when user clicked on (li.contact-nav) and give him class active #4
-$('li.contact-nav').click(function(){
-  var scrollTop         = $(document).scrollTop()
-      heightNav         = $('nav').outerHeight(true)
-      contactOffset     = $('.contact').offset().top - heightNav // 550 + 3892 = 4442
-  $('html, body').animate({scrollTop: contactOffset}, 1000)
-  $('li').removeClass('active')
-  $(this).addClass('active')
-})
 ///////////////////////////
 // When user Clicked on pars-menu
 $('.pars-menu').click(function(){
@@ -85,3 +21,69 @@ $('.menu-phone i.close').click(function(){
   $('.menu-phone').css('transform', 'translateY(-700px)')
   $('.pars-menu').css({'transform': 'scale(1)'})
 })
+
+
+
+
+
+
+
+
+
+
+// CODE SOURCE: https://codepen.io/joxmar/pen/NqqMEg?editors=1010
+// Cache selectors
+var lastId,
+topMenu = $("nav > ul"),
+topMenuHeight = topMenu.outerHeight()+1,
+// All list items
+menuItems = topMenu.find("a"),
+// Anchors corresponding to menu items
+scrollItems = menuItems.map(function(){
+  var item = $($(this).attr("href"));
+   if (item.length) { return item; }
+});
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+ var href = $(this).attr("href"),
+     offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+ $('html, body').stop().animate({ 
+     scrollTop: offsetTop
+ }, 850);
+ e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function(){
+  // Get container scroll position
+  var fromTop = $(this).scrollTop()+topMenuHeight;
+  
+  // Get id of current scroll item
+  var cur = scrollItems.map(function(){
+    if ($(this).offset().top < fromTop)
+      return this;
+  });
+  // Get the id of the current element
+  cur = cur[cur.length-1];
+  var id = cur && cur.length ? cur[0].id : "";
+  
+  if (lastId !== id) {
+      lastId = id;
+      // Set/remove active class
+      menuItems
+        .parent().removeClass("active")
+        .end().filter(`[href="#${id}"]`).parent().addClass("active");
+  }                   
+});
+
+
+
+
+
+
+
+
+
+
